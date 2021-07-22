0939bc548ab11ee81a4e96fdf6ae447527f34ac3
Santiago Dangond <santiago.dangond18@gmail.com>
2021-07-22 11:34:23 -0500

    bug deprecades fixed !! , add comparing module to find terms a his categories

--- comparing.py
+++ comparing.py
@@ -1,1 +1,15 @@
 """ comparar el historial de terminos con los terminos soportados actualmente y saber en que categoria se encuentran"""
+history_tems = open('list_history.json', 'r').read()
+categories_terms = open('categories.json', 'r').read()
+
+import json
+by_categories = json.loads(categories_terms)
+by_history = json.loads(history_tems)
+
+print(by_categories)
+for name, attrs in by_history.items():
+    term = name[4:] 
+    for categorie, items in by_categories.items():
+        if term in items:
+            print(f'{term} is in {categorie} categorie')
+    
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ names_terms = []
 
 theads = soup.find_all('thead')
 for x in theads:
-    names_terms.append(x.text.strip()[11:])
+    names_terms.append(x.text.strip()[11:]) # podria refactorizar esto con una expresion regular, pero aja
 names_terms.pop(0) # para quitar la primera tabla que es la descripcion de los namespaces
 
 tbodys = soup.find_all("tbody")
@@ -22,32 +22,28 @@ for x in tbodys:
 td_list.pop(0)# para quitar la primera tabla que es la descripcion de los namespaces
 
 
-keys = []
-values = []
-prop_terms = []
 FLAT = True
+d = [] 
 for td in td_list:
-    for index in range(len(td)):
+    keys = []
+    values = []
+    for elem in td:
         if FLAT:
-            if td[index].text == "":
-                keys.append("deprecated")
+            if elem.text == '':
+                keys.append('Deprecated')
+                #count +=  # si funciona
             else:
-                keys.append(td[index].text)
-            FLAT = not FLAT
+                keys.append(elem.text)
         else:
-            values.append(td[index].text)
-            FLAT = not FLAT
-    prop_terms.append(dict(zip(keys,values)))
+            values.append(elem.text)
+        FLAT = not FLAT
+    d.append(dict(zip(keys, values)))
+    
+dict_terms_history = dict(zip(names_terms, d))
+
+    
     
 
-to_json_data = { names_terms[x] : prop_terms[x] for x in range(len(names_terms)) }
-print(len(to_json_data.keys()))
-
-
-#js = json.dumps(dict(zip(names_terms, obj)))
-js = json.dumps(to_json_data)
-
-"""
-#json_data = json.loads(js)
-#print(len(json_data))
-"""
+js = json.dumps(dict_terms_history)
+with open ('list_history.json', 'w') as jsonFile:
+    jsonFile.write(js)
